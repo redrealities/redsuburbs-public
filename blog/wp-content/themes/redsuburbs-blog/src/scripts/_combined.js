d043d8f9b00eb9ca9247b2ea38b53965997cc18f
Alex Chernov <alexusblack@gmail.com>
2022-08-19 15:57:52 +1000

    latest blog

---
+++ blog/wp-content/themes/redsuburbs-blog/src/scripts/_combined.js
@@ -0,0 +1,18 @@
+
+/* === File: landing-prime.js === */
+(function() {
+  serverless_mailer.setup({
+    apiUrl: 'https://gblslv2ixl5amw6z2bexp3mjcq0hnahs.lambda-url.us-east-1.on.aws/'
+  });
+  const forms = Array.from(document.querySelectorAll('.serverless-submit__box'));
+  forms.forEach(form => {
+    const successEl = form.querySelector('.serverless-submit__success');
+    const errorEl = form.querySelector('.serverless-submit__error');
+    serverless_mailer.handleForm(
+      form,
+      'RedSuburbs landing page message',
+      () => successEl.textContent = 'We\'ll be in touch soon',
+      error => errorEl.textContent = error
+    );
+  });
+})();
